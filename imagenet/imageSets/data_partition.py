4b78a1016cd4f05653c70445c279e1caa0fc5c7f
Hongji-Lin <973295056@qq.com>
2022-11-03 14:41:26 +0800

    add val files

--- imagenet/imageSets/data_partition.py
+++ imagenet/imageSets/data_partition.py
@@ -44,7 +44,7 @@ def move_img(full_fileDir, empty_fileDir, full_valDir, empty_valDir):
         shutil.move(full_fileDir + full_name, full_valDir)
     for empty_name in empty_sample:
         shutil.move(empty_fileDir + empty_name, empty_valDir)
-    return
+    # return
 
 
 # def move_label(file_list, file_label_train, file_label_val):
@@ -58,17 +58,17 @@ def move_img(full_fileDir, empty_fileDir, full_valDir, empty_valDir):
 
 
 if __name__ == '__main__':
-    # Linux绝对路径
-    full_fileDir = '/root/projects/HRNet-Image-Classification/imagenet/images/train/full/'  # full源图片文件夹路径
-    empty_fileDir = '/root/projects/HRNet-Image-Classification/imagenet/images/train/empty/'  # empty源图片文件夹路径
-    full_valDir = '/root/projects/HRNet-Image-Classification/imagenet/images/val/full/'  # full图片移动到新的文件夹路径
-    empty_valDir = '/root/projects/HRNet-Image-Classification/imagenet/images/val/empty/'  # empty图片移动到新的文件夹路径
+    # # Linux绝对路径
+    # full_fileDir = '/root/projects/HRNet-Image-Classification/imagenet/images/train/full/'  # full源图片文件夹路径
+    # empty_fileDir = '/root/projects/HRNet-Image-Classification/imagenet/images/train/empty/'  # empty源图片文件夹路径
+    # full_valDir = '/root/projects/HRNet-Image-Classification/imagenet/images/val/full/'  # full图片移动到新的文件夹路径
+    # empty_valDir = '/root/projects/HRNet-Image-Classification/imagenet/images/val/empty/'  # empty图片移动到新的文件夹路径
 
-    # # pycharm控制台相对路径
-    # full_fileDir = './imagenet/images/train/full/'  # full源图片文件夹路径
-    # empty_fileDir = './imagenet/images/train/empty/'  # empty源图片文件夹路径
-    # full_valDir = './imagenet/images/val/full/'  # full图片移动到新的文件夹路径
-    # empty_valDir = './imagenet/images/val/empty/'  # empty图片移动到新的文件夹路径
+    # pycharm控制台相对路径
+    full_fileDir = './imagenet/images/train/full/'  # full源图片文件夹路径
+    empty_fileDir = './imagenet/images/train/empty/'  # empty源图片文件夹路径
+    full_valDir = './imagenet/images/val/full/'  # full图片移动到新的文件夹路径
+    empty_valDir = './imagenet/images/val/empty/'  # empty图片移动到新的文件夹路径
 
     # # pycharm直接运行的相对路径
     # full_fileDir = '../images/train/full/'  # full源图片文件夹路径
