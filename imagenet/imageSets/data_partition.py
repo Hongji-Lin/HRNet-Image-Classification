eff5184466b9e300c13105e66c86b4b2a81a7f74
Hongji-Lin <973295056@qq.com>
2022-11-03 14:59:50 +0800

    打印语句

--- imagenet/imageSets/data_partition.py
+++ imagenet/imageSets/data_partition.py
@@ -22,23 +22,25 @@ def move_img(full_fileDir, empty_fileDir, full_valDir, empty_valDir):
     else:
         for name in reemp_list:
             shutil.move(empty_valDir + name, empty_fileDir)
-    return  # 本地调试时要return(清理照片，val保持空的上传），放到服务器上不需要
+    # return  # 本地调试时要return(清理照片，val保持空的上传），放到服务器上不需要
 
     full_list = os.listdir(full_fileDir)  # 取full图片的原始路径
-    print(full_list)
     full_total = len(full_list)
-    print(full_total)
-
     empty_list = os.listdir(empty_fileDir)  # 取empty图片的原始路径
-    print(empty_list)
     empty_total = len(empty_list)
-    print(empty_total)
 
     rate = 0.1  # 自定义抽取图片的比例，比方说100张抽10张，那就是0.1
     full_nums = int(full_total * rate)  # 按照rate比例从full文件夹中取一定数量图片
     empty_nums = int(empty_total * rate)  # 按照rate比例从empty文件夹中取一定数量图片
     full_sample = random.sample(full_list, full_nums)  # 随机选取full数量的样本图片
     empty_sample = random.sample(empty_list, empty_nums)  # 随机选取empty数量的样本图片
+
+    print("按照train:val = 0.9:0.1的比例划分：")
+    print("train集中full的数量为：{}：".format(full_total - full_nums))
+    print("train集中empty的数量为：{}".format(empty_total - empty_nums))
+    print("val集中full的数量为：{}".format(full_nums))
+    print("val集中empty的数量为：{}".format(empty_nums))
+    print("训练集，验证集划分完成！")
 
     for full_name in full_sample:
         shutil.move(full_fileDir + full_name, full_valDir)
