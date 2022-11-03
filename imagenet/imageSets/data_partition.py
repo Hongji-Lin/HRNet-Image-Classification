ed43187de97be36b5b7918dcaa16d4baa4f5e785
Hongji-Lin <973295056@qq.com>
2022-11-03 14:43:55 +0800

    1

--- imagenet/imageSets/data_partition.py
+++ imagenet/imageSets/data_partition.py
@@ -58,13 +58,7 @@ def move_img(full_fileDir, empty_fileDir, full_valDir, empty_valDir):
 
 
 if __name__ == '__main__':
-    # # Linux绝对路径
-    # full_fileDir = '/root/projects/HRNet-Image-Classification/imagenet/images/train/full/'  # full源图片文件夹路径
-    # empty_fileDir = '/root/projects/HRNet-Image-Classification/imagenet/images/train/empty/'  # empty源图片文件夹路径
-    # full_valDir = '/root/projects/HRNet-Image-Classification/imagenet/images/val/full/'  # full图片移动到新的文件夹路径
-    # empty_valDir = '/root/projects/HRNet-Image-Classification/imagenet/images/val/empty/'  # empty图片移动到新的文件夹路径
-
-    # pycharm控制台相对路径
+    # Linux/pycharm控制台相对路径
     full_fileDir = './imagenet/images/train/full/'  # full源图片文件夹路径
     empty_fileDir = './imagenet/images/train/empty/'  # empty源图片文件夹路径
     full_valDir = './imagenet/images/val/full/'  # full图片移动到新的文件夹路径
