d626350818a094610c8aa5ce0ecf7dededa98335
Hongji-Lin <973295056@qq.com>
2022-11-03 10:55:44 +0800

    3

--- imagenet/imageSets/data_partition.py
+++ imagenet/imageSets/data_partition.py
@@ -39,8 +39,8 @@ def move_img(full_fileDir, empty_fileDir, full_valDir, empty_valDir):
 if __name__ == '__main__':
     full_fileDir = 'imagenet/images/train/full/'  # full源图片文件夹路径
     empty_fileDir = 'imagenet/images/train/empty/'  # empty源图片文件夹路径
-    full_valDir = '../val/full/'  # full图片移动到新的文件夹路径
-    empty_valDir = '..val/empty/'  # empty图片移动到新的文件夹路径
+    full_valDir = 'imagenet/images/val/full/'  # full图片移动到新的文件夹路径
+    empty_valDir = 'imagenet/images/val/empty/'  # empty图片移动到新的文件夹路径
     move_img(full_fileDir, empty_fileDir, full_valDir, empty_valDir)
     # file_list = os.listdir(tarDir)
     # file_label_train = r"E:\NEt\yolov5-hat\VOCdevkit\labels\train"  # 源图片标签路径
